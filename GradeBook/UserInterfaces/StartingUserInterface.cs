4a773a962edcc7664a305dd8e54888ba703d5519
joakimmalmstrom91 <malmstrom_9@hotmail.com>
2020-08-31 11:43:52 +0200

    isWeighted parameter

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -41,16 +41,17 @@ namespace GradeBook.UserInterfaces
             }
             var name = parts[1];
             var type = parts[2];
+            var weighted = parts[3];
 
             BaseGradeBook gradeBook;
 
             if (type == "standard")
             {
-                gradeBook = new StandardGradeBook(name);
+                gradeBook = new StandardGradeBook(name, weighted);
             }
             else if (type == "ranked")
             {
-                gradeBook = new RankedGradeBook(name);
+                gradeBook = new RankedGradeBook(name, weighted);
             }
             else
             {
